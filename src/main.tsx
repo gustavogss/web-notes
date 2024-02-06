56008529fd76028ccbe7a1edb2e73a722ded1306
gustavogss <gustavogss.dev@gmail.com>
2024-02-05 23:36:25 -0300

    feat: create home

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,10 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import {App} from './app'
 import './index.css'
+import { Home } from './screens/Home'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+    <Home />
   </React.StrictMode>,
 )
